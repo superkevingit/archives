b038302277f8345bd134402c6334b06b8a028fe4
superkevingit <superkevingit@gmail.com>
2016-05-30 20:02:47 +0800

    demo compelete

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 # encoding: utf-8
 import requests
+from bs4 import BeautifulSoup
 
 
 head = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36',
@@ -11,4 +12,5 @@ data = {
     'idcardNo': ""
 }
 html = requests.post(url, data=data, headers=head)
-print(html.text)
+soup = BeautifulSoup(html.text, "html.parser")
+xy = soup.find(id="XY")["value"]
